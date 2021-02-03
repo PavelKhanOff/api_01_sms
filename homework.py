15793c67e8c47cbbf4b96313fe47e7f5fc9706c2
Pavel Khan <Pavelkhanoff@gmail.com>
2021-02-04 00:56:52 +0600

    1st with errors

--- homework.py
+++ homework.py
@@ -7,19 +7,16 @@ from dotenv import load_dotenv
 from twilio.rest import Client
 
 load_dotenv()
-
 logging.basicConfig(
     level=logging.DEBUG,
     filename='main.log',
     format='%(asctime)s, %(levelname)s, %(message)s, %(name)s'
 )
-
 ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
 AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
 NUMBER_FROM = os.getenv('NUMBER_FROM')
 NUMBER_TO = os.getenv('NUMBER_TO')
 CLIENT = Client(ACCOUNT_SID, AUTH_TOKEN)
-
 BASE_URL = 'https://api.vk.com/method/users.get'
 ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
 API_V = '5.89'
@@ -39,8 +36,12 @@ def get_status(user_id):
     try:
         status = requests.post(BASE_URL, params=params).json()['response']
         return status[0]['online']
+    except ValueError:
+        logging.exception(msg='Ошибка в работе json')
+    except ConnectionError:
+        logging.exception(msg='Ошибка соединения')
     except Exception:
-        logging.exception(msg='Ошибка в работе отправки сообщения!')
+        logging.exception(msg='Что-то сломалось:)')
 
 
 def send_sms(sms_text):
